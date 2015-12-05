8d954df97585f95e40744ae05c176ce72c581967
Jorge Henrique de S. Rocha <jhrocha.sousa@gmail.com>
2015-12-05 11:32:37 -0300

    Base service generator was integrated

--- ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Viewcontroller/Services/Base/ServiceGenerator.java
+++ ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Viewcontroller/Services/Base/ServiceGenerator.java
@@ -1,5 +1,22 @@
-package paymentcom.parking.jorge.parkingpayment.Viewcontroller.;
+package paymentcom.parking.jorge.parkingpayment.Viewcontroller.Services.Base;
+
+import com.squareup.okhttp.OkHttpClient;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
 
 public class ServiceGenerator {
+
+    public static final String API_BASE_PATH = "";
+    private static OkHttpClient okHttpClient = new OkHttpClient();
+    private static Retrofit.Builder builder= new Retrofit.Builder()
+            .baseUrl(API_BASE_PATH)
+            .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass){
+        Retrofit retrofit= builder.client(okHttpClient).build();
+        return retrofit.create(serviceClass);
+    }
+
 
 }
