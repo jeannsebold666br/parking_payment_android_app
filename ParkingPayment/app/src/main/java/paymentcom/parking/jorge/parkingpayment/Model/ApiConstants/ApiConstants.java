7b3d45f47aa656d012e38c353d8b64e50223042c
Jorge Henrique de S. Rocha <jhrocha.sousa@gmail.com>
2015-12-05 11:52:58 -0300

    Added public constants for requests

---
+++ ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Model/ApiConstants/ApiConstants.java
@@ -0,0 +1,8 @@
+package paymentcom.parking.jorge.parkingpayment.Model.ApiConstants;
+
+/**
+ * Created by jorgehsrocha on 12/5/15.
+ */
+public final class ApiConstants {
+    public static String BASE_URL="http://pk.meuvalor.com/api/v1";
+}
--- ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Viewcontroller/Services/Base/ServiceGenerator.java
+++ ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Viewcontroller/Services/Base/ServiceGenerator.java
@@ -2,12 +2,13 @@ package paymentcom.parking.jorge.parkingpayment.Viewcontroller.Services.Base;
 
 import com.squareup.okhttp.OkHttpClient;
 
+import paymentcom.parking.jorge.parkingpayment.Model.ApiConstants.ApiConstants;
 import retrofit.GsonConverterFactory;
 import retrofit.Retrofit;
 
 public class ServiceGenerator {
 
-    public static final String API_BASE_PATH = "";
+    public static final String API_BASE_PATH = ApiConstants.BASE_URL;
     private static OkHttpClient okHttpClient = new OkHttpClient();
     private static Retrofit.Builder builder= new Retrofit.Builder()
             .baseUrl(API_BASE_PATH)
