fd2bbb6edc7f16af71cc710701417df561e52cbc
Jorge Henrique de S. Rocha <jhrocha.sousa@gmail.com>
2015-12-08 22:06:57 -0300

    Get all tickets - request

--- ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Viewcontroller/Services/Requests/Ticket/TicketRequest.java
+++ ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/Viewcontroller/Services/Requests/Ticket/TicketRequest.java
@@ -1,7 +1,16 @@
 package paymentcom.parking.jorge.parkingpayment.Viewcontroller.Services.Requests.Ticket;
 
+import java.util.List;
+
+import paymentcom.parking.jorge.parkingpayment.Model.Ticket.TicketResponse;
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.POST;
+
 /**
  * Created by jorgehsrocha on 12/8/15.
  */
-public class TicketRequest {
+public interface TicketRequest {
+    @GET("/api/v1/ticket/all")
+    Call<List<TicketResponse>> allTickets();
 }
