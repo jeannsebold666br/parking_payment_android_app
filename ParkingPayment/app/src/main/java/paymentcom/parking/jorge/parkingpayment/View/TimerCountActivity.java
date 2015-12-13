ccd3b6e5f92e7ac2ae6945e10fbb2ccd643bca06
Jorge Henrique de S. Rocha <jhrocha.sousa@gmail.com>
2015-12-13 18:43:40 -0300

    Final version of visual

--- ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/View/TimerCountActivity.java
+++ ParkingPayment/app/src/main/java/paymentcom/parking/jorge/parkingpayment/View/TimerCountActivity.java
@@ -66,7 +66,7 @@ public class TimerCountActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_timer_count, menu);
+//        getMenuInflater().inflate(R.menu.menu_timer_count, menu);
         return true;
     }
 
